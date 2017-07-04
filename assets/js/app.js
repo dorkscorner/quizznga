b74416e88a8a4ac905f08c42ceb7bcb7d5bbd35d
Devi Prasad <deviprsd21@gmail.com>
2017-07-05 00:40:32 +0530

    0.0.0.1240752017

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 +(function(){
     $(document).ready(function() {
-	    $('#fullpage').fullpage();
+	    $('#fullpage').fullpage({
+            'verticalCentered': false,
+            'continuousVertical': true,
+            'css3': true,
+            'navigation': true,
+            'navigationPosition': 'right',
+            'navigationTooltips': ['', 'About Us', 'Quizznga 2017', 'Registration Form', 'Sponsers', 'Team', 'Contact Us'],
+        });
     });
 })()
